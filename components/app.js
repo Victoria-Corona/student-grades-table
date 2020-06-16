61c9b679f5f7259f83396915fa4670fe65bf51c7
Victoria Corona <corona.victoria@hotmail.com>
2020-06-16 15:02:54 -0700

    created a series of methods to go within the App class

--- components/app.js
+++ components/app.js
@@ -1,1 +1,26 @@
-class App
+class App{
+  constructor(){
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+  handleGetGradesError(error){
+    console.error("nope", error);
+  }
+  handleGetGradesSuccess(grades){
+    console.log("yes", grades)
+  }
+  getGrades(){
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      Headers: {
+        "X-Access-Token": "Dcw6eCfN"
+      },
+      success: handleGetGradesSuccess,
+      error: handleGetGradesError
+    })
+  }
+  start(){
+    this.getGrades();
+  }
+}
