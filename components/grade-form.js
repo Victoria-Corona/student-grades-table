a09c55c6901a920b43ed6209a3886c5aea079bf9
Victoria Corona <corona.victoria@hotmail.com>
2020-06-17 10:49:54 -0700

    made slight correction

--- components/grade-form.js
+++ components/grade-form.js
@@ -1,7 +1,9 @@
 class GradeForm{
   constructor(formElement){
     this.formElement = formElement;
-    this.formElement = this.handleSubmit.bind(submit)
+    this.formElement.addEventListener("submit", this.handleSubmit);
+
+    // this.formElement = this.handleSubmit.bind(submit)
   }
   onSubmit(createGrade){
     this.createGrade = createGrade;
